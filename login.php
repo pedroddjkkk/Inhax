c735fba1b43fd961a5d228f21f655c3a0863cae8
Pedro Valério <pedroddjkK@gmail.com>
2023-08-27 18:43:26 -0300

    Arquivos PHP

---
+++ login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="lib/styles/login.css" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
+    integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <title>InhaX</title>
+</head>
+
+<body>
+  <div class="login">
+    <div class="form-wrapper">
+      <form action="">
+        <h1>Login</h1>
+        <div class="text-field">
+          <label for="username">Usuário</label>
+          <input type="text" placeholder="" name="username" />
+        </div>
+        <div class="text-field" style="margin-top: 20px">
+          <label for="password">Senha</label>
+          <input type="text" placeholder="" name="password" />
+        </div>
+        <div class="inline">
+          <span id="login-error-alert" class="hide">Senha ou Usuário incorreto!</span>
+          <a class="forget-password">Esqueci minha senha</a>
+        </div>
+
+        <a class="forget-password" style="margin: 0 0 16px 0;" href="registro.html" id="register">Não tem uma conta?
+          Registre-se!</a>
+        <button id="form-submit" class="login-button">Entrar</button>
+      </form>
+    </div>
+    <img src="images/image-login.png" alt="Foguete" />
+  </div>
+</body>
+<script>
+  $(document).ready(function () {
+    $("#form-submit").click(function (event) {
+      event.preventDefault();
+      $("#login-error-alert").removeClass("hide");
+      $("#login-error-alert").addClass("show");
+    });
+  });
+</script>
+
+</html>
