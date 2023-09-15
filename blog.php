6d754ff16c82940b5bc1bf03f38008c42558ebb9
Pedro Aparecido Valério <pedroddjkk@gmail.com>
2023-09-15 09:26:02 -0300

    Using WYSIWYG tinymce from Composer

--- blog.php
+++ blog.php
@@ -9,11 +9,19 @@ session_start();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="lib/styles/style.css" rel="stylesheet" />
+  <script src="/vendor/tinymce/tinymce/tinymce.min.js" referrerpolicy="origin"></script>
+  <script>
+      tinymce.init({
+        selector: '#mytextarea',
+        language: "pt_BR"
+      });
+    </script>
   <title>InhaX</title>
 </head>
 
 <body>
   <?php include "components/navbar.php" ?>
+  <textarea id="mytextarea">Hello, World!</textarea>
 </body>
 
 </html>
