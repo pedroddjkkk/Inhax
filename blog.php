472d8f41e795894b9df2523349abdbcf3a6b26f0
Pedro Aparecido Valério <pedroddjkk@gmail.com>
2023-09-25 08:51:18 -0300

    Blog Working

--- blog.php
+++ blog.php
@@ -4,18 +4,33 @@ include_once("lib/connection.php");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
+  if (!isset($_SESSION['userId'])) {
+    header("Location: login.php");
+    exit();
+  }
+
   $content = $_POST['content'];
   $title = $_POST['title'];
   $userId = $_SESSION['userId'];
 
+  if (!isset($content) || !isset($title)) {
+    header("Location: blog.php");
+    exit();
+  }
   $sql = "INSERT INTO posts (content, title, user_id) VALUES (?, ?, ?)";
   $stmt = $con->prepare($sql);
   $stmt->bind_param("ssi", $content, $title, $userId);
   $stmt->execute();
 
-  header("Location: index.php");
+  $stmt->close();
+  $con->close();
+
+  header("Location: blog.php");
   exit();
 }
+
+$sql = "SELECT * FROM posts";
+$result = $con->query($sql);
 ?>
 
 <!DOCTYPE html>
@@ -25,12 +40,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="lib/styles/style.css" rel="stylesheet" />
+  <link rel="stylesheet" href="lib/styles/blog.css" />
   <script src="/vendor/tinymce/tinymce/tinymce.min.js" referrerpolicy="origin"></script>
   <script>
     tinymce.init({
       selector: '#mytextarea',
       language: "pt_BR",
-      promotion: false
+      promotion: false,
+      height: "250px"
     });
   </script>
   <title>InhaX</title>
@@ -38,13 +55,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <body>
   <?php include "components/navbar.php" ?>
+  <?php if (isset($_SESSION['userId'])) { ?>
+    <section class="align-center">
+      <form action="blog.php" method="post" class="align-center blog-quill-wrapper" id="blog-post-form">
+        <div class="text-field">
+          <input type="text" id="title-input" name="title" placeholder="Titulo">
+        </div>
+        <textarea id="mytextarea" name="content" style="width: 100%;"></textarea>
+        <input type="submit" name="Enviar" id="">
+      </form>
+    </section>
+  <?php } ?>
+
   <section class="align-center">
-    <form action="blog.php" method="post" class="align-center blog-quill-wrapper">
-      <label for="#title-input">Titulo</label>
-      <input type="text" id="title-input" name="title">
-      <textarea id="mytextarea" name="content"></textarea>
-      <input type="submit" name="Enviar" id="">
-    </form>
+    <h2>Posts</h2>
+    <ul>
+      <?php while ($row = $result->fetch_assoc()) { ?>
+        <li>
+          <h3>
+            <?php echo $row['title']; ?>
+          </h3>
+          <p>
+            <?php echo $row['content']; ?>
+          </p>
+        </li>
+      <?php } ?>
+    </ul>
   </section>
 </body>
 
