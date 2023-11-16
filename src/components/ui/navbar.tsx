fda51678e05b77436873c14b15cc309d58e64ce5
Pedro Aparecido Valério <pedroddjkk@gmail.com>
2023-11-16 16:18:06 -0300

    Add slide animation and update user name display in navbar

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Slide } from "@mui/material";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
 
@@ -55,46 +56,48 @@ export default function Navbar() {
                 />{" "}
               </g>
             </svg>
-            <a>Danillo Pitta</a>
+            <a>{session.data?.user?.name}</a>
           </div>
         </ul>
       </div>
-      <div className="lateral-account-nav hide" id="lateral-account-nav">
-        <div className="line" style={{ justifyContent: "space-between" }}>
-          <h2></h2>
-          <a onClick={() => setOpen(false)} className="icon">
-            <svg
-              width="24px"
-              height="24px"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              stroke="#ffffff"
-            >
-              <g id="SVGRepo_bgCarrier" strokeWidth="0" />
-              <g
-                id="SVGRepo_tracerCarrier"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <g id="SVGRepo_iconCarrier">
-                <path
-                  d="M20.7457 3.32851C20.3552 2.93798 19.722 2.93798 19.3315 3.32851L12.0371 10.6229L4.74275 3.32851C4.35223 2.93798 3.71906 2.93798 3.32854 3.32851C2.93801 3.71903 2.93801 4.3522 3.32854 4.74272L10.6229 12.0371L3.32856 19.3314C2.93803 19.722 2.93803 20.3551 3.32856 20.7457C3.71908 21.1362 4.35225 21.1362 4.74277 20.7457L12.0371 13.4513L19.3315 20.7457C19.722 21.1362 20.3552 21.1362 20.7457 20.7457C21.1362 20.3551 21.1362 19.722 20.7457 19.3315L13.4513 12.0371L20.7457 4.74272C21.1362 4.3522 21.1362 3.71903 20.7457 3.32851Z"
-                  fill="#ffffff"
+      <Slide direction="left" in={open} mountOnEnter unmountOnExit>
+        <div className="lateral-account-nav" id="lateral-account-nav">
+          <div className="line" style={{ justifyContent: "space-between", justifyItems: "center" }}>
+            <h2 className="mt-2">{session.data?.user?.name}</h2>
+            <a onClick={() => setOpen(false)} className="icon">
+              <svg
+                width="24px"
+                height="24px"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="#ffffff"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
-              </g>
-            </svg>
-          </a>
+                <g id="SVGRepo_iconCarrier">
+                  <path
+                    d="M20.7457 3.32851C20.3552 2.93798 19.722 2.93798 19.3315 3.32851L12.0371 10.6229L4.74275 3.32851C4.35223 2.93798 3.71906 2.93798 3.32854 3.32851C2.93801 3.71903 2.93801 4.3522 3.32854 4.74272L10.6229 12.0371L3.32856 19.3314C2.93803 19.722 2.93803 20.3551 3.32856 20.7457C3.71908 21.1362 4.35225 21.1362 4.74277 20.7457L12.0371 13.4513L19.3315 20.7457C19.722 21.1362 20.3552 21.1362 20.7457 20.7457C21.1362 20.3551 21.1362 19.722 20.7457 19.3315L13.4513 12.0371L20.7457 4.74272C21.1362 4.3522 21.1362 3.71903 20.7457 3.32851Z"
+                    fill="#ffffff"
+                  />
+                </g>
+              </svg>
+            </a>
+          </div>
+          <div className="line">
+            <div className="stroke"></div>
+          </div>
+          <div className="line">
+            <a href="/app/logout.php" className="lateral-account-nav-btn">
+              Sair
+            </a>
+          </div>
         </div>
-        <div className="line">
-          <div className="stroke"></div>
-        </div>
-        <div className="line">
-          <a href="/app/logout.php" className="lateral-account-nav-btn">
-            Sair
-          </a>
-        </div>
-      </div>
+      </Slide>
     </nav>
   );
 }
