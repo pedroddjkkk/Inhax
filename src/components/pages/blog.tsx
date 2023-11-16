3b06bb4c6cda3169fabd915ef05ade19f2b2db87
Pedro Aparecido Valério <pedroddjkk@gmail.com>
2023-11-16 18:46:00 -0300

    Add "Add plus icon to blog title for admin users".

--- src/components/pages/blog.tsx
+++ src/components/pages/blog.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Prisma } from "@prisma/client";
 import { useSession } from "next-auth/react";
+import { GoPlus } from "react-icons/go";
 
 export default function Blog({
   posts,
@@ -12,11 +13,16 @@ export default function Blog({
   }>[];
 }) {
   const session = useSession();
-  
+
   return (
     <div>
       <div className="mx-auto w-[80%] bg-slate-50 flex flex-col justify-center items-center shadow-sm mt-8 p-8">
-        <h1 className="text-4xl font-semibold">Blog</h1>
+        <h1 className="text-4xl font-semibold">
+          Blog{" "}
+          {session.data?.user.admin ? (
+            <GoPlus size={24} className="text-red-500 ml-2" />
+          ) : null}
+        </h1>
         <span className="text-lg mt-1">
           Aqui está as ultimas atualizações de viagens espaciais!
         </span>
